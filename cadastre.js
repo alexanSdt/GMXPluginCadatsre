aefe803c8257e095cc36c5257619a33ef1df02b2
Zemledelec <zemledelec@openglobus.org>
2014-07-30 18:02:29 +0400

    Сделал инфо. окно для поискв по кадастровому номеру такое-же как и для клика.

--- cadastre.js
+++ cadastre.js
@@ -441,11 +441,11 @@
     };
 
     function searchOkrugDate(okrugNumber) {
-
+        //TODO: возможно не требуется
     };
 
     function searchZone(point) {
-
+        //TODO: возможно не требуется
     };
 
     function formatDate(d, m, y) {
@@ -616,20 +616,6 @@
         //if (!feature.attributes[FIELDS.geometryError]) {
         //    content += substitute(INFO_WINDOW_CONTENT_TEMPLATE.cadastreMapInfoPlansRow, [feature.attributes['PARCEL_CN']]);
         //}
-
-        //if (feature.attributes[FIELDS.parcelId] == '52:18:70020:25') {
-        //    content += substitute(INFO_WINDOW_CONTENT_TEMPLATE.cadastreMapInfo3DRow, ['index_apartmentcomplex.html']);
-        //}
-        //else if (feature.attributes[FIELDS.parcelId] == '52:18:60085:21') {
-        //    content += substitute(INFO_WINDOW_CONTENT_TEMPLATE.cadastreMapInfo3DRow, ['index_gaspipeline.html']);
-        //}
-        //else if (feature.attributes[FIELDS.parcelId] == '52:18:70012:23') {
-        //    content += substitute(INFO_WINDOW_CONTENT_TEMPLATE.cadastreMapInfo3DRow, ['index_teledom.html']);
-        //}
-
-        //content += INFO_WINDOW_CONTENT_TEMPLATE.mapInfoTableFooter;
-        //content += INFO_WINDOW_CONTENT_TEMPLATE.mapInfoContainerFooter;
-        //content += INFO_WINDOW_CONTENT_TEMPLATE.tabFooter;
         //End "MapInfo"
 
         //Start "Info"
@@ -655,9 +641,11 @@
 
         //Start "Zone"
         //content += INFO_WINDOW_CONTENT_TEMPLATE.zoneWaitListItem;
+        //searchZone((feature.clickedPoint) ? (feature.clickedPoint) : (feature.point));
         //End "Zone"
 
         ////Start "Link"
+        ////ссылки на получение кадастровых данных
         //content += INFO_WINDOW_CONTENT_TEMPLATE.linkListHeader;
         //for (var linkItem in INFO_WINDOW_CONTENT_TEMPLATE.linkListItems) {
         //    content += substitute(INFO_WINDOW_CONTENT_TEMPLATE.linkListItems[linkItem], [feature.attributes["PARCEL_CN"], KINDS[-1]]);
@@ -668,14 +656,11 @@
         content += INFO_WINDOW_CONTENT_TEMPLATE.mapInfoTableFooter;
         content += INFO_WINDOW_CONTENT_TEMPLATE.mapInfoContainerFooter;
 
-
-        //searchZone((feature.clickedPoint) ? (feature.clickedPoint) : (feature.point));
-
         return content;
     };
 
     function buildOksInfoWindowContent(feature) {
-
+        return "Если вы увидели это сообщение, пожалуйста обратитесь в службу поддержки космоснимков, предварительно сделайте снимок карты, и запишите кадастровый номер.";
     };
 
 
@@ -1153,10 +1138,14 @@
     }
 
     var cadastreSearch = function (map, value) {
+
         if (checkCadastreNumber(value)) {
+
             var cadType = getCadastreType(value);
+
             $('#loader').show();
             $("#alert").hide();
+
             if (cadType == CadastreTypes.parcel) {
                 $.ajax(cadType.layerUrl, {
                     crossDomain: true,
@@ -1168,66 +1157,31 @@
                     data: {
                         cadNum: value,
                         onlyAttributes: 'false',
-                        returnGeometry: 'true',
+                        returnGeometry: 'false',
                         f: 'json'
                     }
                 }).done(function (data) {
                     $('#loader').hide();
-                    var x = converting(data.features[0].attributes.XC, "x"), y = converting(data.features[0].attributes.YC, "y"), maxX = converting(data.features[0].attributes.XMAX, "x"), minX = converting(data.features[0].attributes.XMIN, "x"), maxY = converting(data.features[0].attributes.YMAX, "y"), minY = converting(data.features[0].attributes.YMIN, "y");
-                    if (data.features[0].attributes.ERRORCODE != 1) {
-                        map.zoomToExtent(minX, minY, maxX, maxY);
-                        var extent = { minX: minX, minY: minY, maxX: maxX, maxY: maxY };
-                        createBalloonInfo(x, y, extent, "");
-                    } else {
-                        map.zoomToExtent(minX, minY, maxX, maxY);
-                        if (balloonInfo)
-                            balloonInfo.setVisible(false);
-                        if (cadastreLayerInfo)
-                            cadastreLayerInfo.setVisible(false);
-                        var data = data.features[0].attributes;
-                        var html = "<div style='width:300px; height:300px; overflow-x: hidden; overflow-y: scroll;'>";
-                        balloonSearch = map.addBalloon();
-                        balloonSearch.setPoint(x, y);
-                        balloonSearch.setVisible(false);
 
-                        html += "<h3>" + "Кадастровые участки" + "</h3><br><div><table style='text-align:left'>";
-                        html += "<tr><th>Статус: </th><td>" + test(PARCEL_STATES[parseInt(data["PARCEL_STATUS"]) - 1]) + "</td></tr>";
-                        html += "<tr><th>Адрес: </th><td>" + test(data["OBJECT_ADDRESS"]) + "</td></tr>";
-                        html += "<tr><th>Декларированная площадь: </th><td>" + test(data["AREA_VALUE"]) + test(UNITS[data["AREA_UNIT"]]) + "</td></tr>";
-                        html += "<tr><th>Кадастровая стоимость: </th><td>" + test(data["CAD_COST"]) + test(UNITS[data["CAD_UNIT"]]) + "</td></tr>";
-                        html += "<tr><th>Форма собственности: </th><td>" + test(data["RC_TYPE"]) + "</td></tr>";
-                        html += "<tr><th>Дата постановки на учет: </th><td>" + test(parseDate(data["DATE_CREATE"])) + "</td></tr>";
-                        var Num = data["PARCEL_CN"].substr(0, data["PARCEL_CN"].lastIndexOf(":"));
-                        html += "<tr><th>Квартал: </th><td>" + test(Num) + "</td></tr>";
-                        Num = Num.substr(0, Num.lastIndexOf(":"));
-                        html += "<tr><th>Район: </th><td>" + test(Num) + "</td></tr>";
-                        Num = Num.substr(0, Num.lastIndexOf(":"));
-                        html += "<tr><th>Округ: </th><td>" + test(Num) + "</td></tr>";
-                        html += "<tr><th>Дата обновления сведений ПКК: </th><td>" + test(parseDate(data["ACTUAL_DATE"])) + "</td></tr>";
-                        html += "<tr><th>Категория: </th><td>" + test(CATEGORY_TYPES[data["CATEGORY_TYPE"]]) + "</td></tr>";
-                        html += "<tr><th>Разрешенное использование </th><td></td></tr>";
-                        html += "<tr><th>По классификатору (код): </th><td>" + test(data["UTIL_CODE"]) + "</td></tr>";
-                        html += "<tr><th>По классификатору (описание): </th><td>" + test(UTILIZATIONS[data["UTIL_CODE"]]) + "</td></tr>";
-                        html += "<tr><th>По документу: </th><td>" + test(data["UTIL_BY_DOC"]) + "</td></tr>";
-                        html += "</table><br>";
-                        html += '<a target="_blank" href="https://rosreestr.ru/wps/portal/cc_information_online?KN=' + data["PARCEL_CN"] + '">Справочная информация об объекте недвижимости</a><br>';
-                        html += '<a target="_blank" href="https://rosreestr.ru/wps/portal/cc_gkn_form_new?KN=' + data["PARCEL_CN"] + '&objKind=002001001000">Запрос о предоставлении сведений ГКН</a><br>';
-                        html += '<a target="_blank" href="https://rosreestr.ru/wps/portal/cc_egrp_form_new?KN=' + data["PARCEL_CN"] + '&objKind=002001001000">Запрос о предоставлении сведений ЕГРП</a><br>';
-                        html += "</div>";
+                    var x = converting(data.features[0].attributes.XC, "x"),
+                        y = converting(data.features[0].attributes.YC, "y"),
+                        maxX = converting(data.features[0].attributes.XMAX, "x"),
+                        minX = converting(data.features[0].attributes.XMIN, "x"),
+                        maxY = converting(data.features[0].attributes.YMAX, "y"),
+                        minY = converting(data.features[0].attributes.YMIN, "y");
 
-                        balloonSearch.div.innerHTML = html;
-                        balloonSearch.setVisible(true);
-                        balloonSearch.resize();
-                        balloonSearch.addListener('onClose', function (obj) {
-                            cadastreLayerSearch.setVisible(false);
-                        });
-                    }
+                    map.zoomToExtent(minX, minY, maxX, maxY);
+                    createBalloonInfo(x, y, { minX: minX, minY: minY, maxX: maxX, maxY: maxY }, "");
+
                 }).fail(function () {
                     $('#loader').hide();
                     $("#alert").show();
                 });
+
             } else {
+
                 var cadastreNumber = normalizeSearchCadastreNumber(value);
+
                 $.getJSON(cadType.layerUrl + '/query?' + 'where=' + encodeURIComponent(cadType.fieldId + " like '" + cadastreNumber + "%'"), {
                     f: 'json',
                     returnGeometry: true,
@@ -1243,14 +1197,7 @@
                     if (cadastreLayerInfo)
                         cadastreLayerInfo.setVisible(false);
 
-                    if (balloonSearch)
-                        balloonSearch.setVisible(false);
-
-                    if (cadastreLayerSearch)
-                        cadastreLayerSearch.setVisible(false);
-
-                    var geom = getGeometry(data.features[0].geometry.rings);
-                    showGeometry(geom);
+                    geometry = getGeometry(data.features[0].geometry.rings);
 
                     var findInfo = data.features[0].attributes;
 
@@ -1264,45 +1211,14 @@
                     map.zoomToExtent(minX, minY, maxX, maxY);
 
                     var html = "<div style='width:300px; height:300px; overflow-x: hidden; overflow-y: scroll;'>";
-                    balloonSearch = map.addBalloon();
-                    balloonSearch.setPoint(converting(findInfo["XC"], "x"), converting(findInfo["YC"], "y"));
-                    balloonSearch.setVisible(false);
-                    html += "<h3>" + "Кадастровый номер  " + test(findInfo["CAD_NUM"]) + "</h3><br><div><table style='text-align:left'>";
-                    html += "<tr><th>Дата обновления сведений ПКК: </th><td> " + test(parseDate(findInfo["ACTUAL_DATE"])) + "</td></tr>";
-                    html += "<tr><th>Кадастровый номер: </th><td> " + test(findInfo["CAD_NUM"]) + "</td></tr>";
-                    html += "<tr><th>Имя: </th><td> " + test(findInfo["NAME"]) + "</td></tr>";
-                    html += "<tr><th>Идентификатор объекта: </th><td> " + test(findInfo["OBJECTID"]) + "</td></tr>";
-                    html += "<tr><th>Идентификатор ПКК: </th><td> " + test(findInfo["PKK_ID"]) + "</td></tr>";
-                    html += "<tr><th>Ключ региона: </th><td> " + test(findInfo["REGION_KEY"]) + "</td></tr>";
-                    html += "<tr><th>X центра: </th><td> " + test(findInfo["XC"]) + "</td></tr>";
-                    html += "<tr><th>Y центра: </th><td> " + test(findInfo["YC"]) + "</td></tr>";
-                    html += "<tr><th>Макс. X: </th><td> " + test(findInfo["XMAX"]) + "</td></tr>";
-                    html += "<tr><th>Мин. X: </th><td> " + test(findInfo["XMIN"]) + "</td></tr>";
-                    html += "<tr><th>Макс. Y: </th><td> " + test(findInfo["YMAX"]) + "</td></tr>";
-                    html += "<tr><th>Мин. Y: </th><td> " + test(findInfo["YMIN"]) + "</td></tr>";
-                    html += "</table><br>";
-                    html += '<br /><span style="cursor: pointer; text-decoration: underline;" class="getGeom" >Получить геометрию</span>'
-                    html += "</div>";
-                    balloonSearch.div.innerHTML = html;
-                    balloonSearch.setVisible(true);
-                    balloonSearch.resize();
-                    cadastreLayerSearch.setVisible(true);
-                    balloonSearch.addListener('onClose', function (obj) {
-                        cadastreLayerSearch.setVisible(false);
-                    });
+                    balloonInfo = map.addBalloon();
+                    balloonInfo.setPoint(converting(findInfo["XC"], "x"), converting(findInfo["YC"], "y"));
+                    balloonInfo.setVisible(false);
 
-                    $(".getGeom").click(function () {
-                        var result = JSON.stringify([{
-                            "properties": { "isVisible": true, "text": "" },
-                            "geometry": geom
-                        }]);
-                        sendCrossDomainPostRequest(serverBase + "Shapefile.ashx", {
-                            name: cadastreNumber,
-                            format: "Shape",
-                            points: '',
-                            lines: '',
-                            polygons: result
-                        });
+                    showInfoWindow(cadType, data.features[0]);
+
+                    balloonInfo.addListener('onClose', function (obj) {
+                        cadastreLayerInfo.setVisible(false);
                     });
 
                 }).fail(function () {
